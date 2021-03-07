737c2c62ba9bec9390f594b6a626e4afc1d6d8bf
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-07 19:17:04 +0900

    resolve canvas px error

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 500;
+canvas.height = 500;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -15,16 +18,19 @@ function startPainting() {
 }
 
 function onMouseMove(event) {
+  // 마우스를 움직이는 내내 작동
   // console.log(event);
   const x = event.offsetX;
   const y = event.offsetY;
   // console.log(x, y);
 
   if (!painting) {
+    // console.log("creating path in", x, y);
     // 클릭하고 움직이면 더이상 실행되지 않음
     ctx.beginPath(); // 클락하지 않고 떠다님(클릭하기를 기다림) 선의 시작점을 만들기 위함
     ctx.moveTo(x, y);
   } else {
+    // console.log("creating line in", x, y);
     ctx.lineTo(x, y); //path의 마지막 점에 연결 그래서 path가 먼저 만들어 져야됨
     ctx.stroke();
   }
